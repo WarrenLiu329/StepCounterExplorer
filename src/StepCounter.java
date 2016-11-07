ca8115ab9ee56f63cf3a443806b5154175ab7fb6
L.B. Warren Liu <warrenliu403@gmail.com>
2016-11-06 22:03:43 -0800

    change the count steps to get new threshold

--- src/StepCounter.java
+++ src/StepCounter.java
@@ -1,27 +1,30 @@
 
 public class StepCounter {
-
-	public static double[] countSteps(double[] times, double[][] sensorData) {
-		double[] steps = new double[times.length];
+//aaaaa
+	public static int countSteps(double[][] sensorData) {
+		int steps = 0;
 		double[] magnitudes = calculateMagnitudesFor(sensorData);
-		double mean = calculateMean(magnitudes);
-		double standardDeviation = calculateStandardDeviation(magnitudes, mean);
-		for (int i = 1; i < magnitudes.length - 1; i++) {
-			//System.out.println(isPeak(magnitudes, i));
-		//	System.out.println(magnitudes.length);
-			//System.out.println(magnitudes[i]);
-			if (isPeak(magnitudes, i)) {
-				if (magnitudes[i] >= mean + standardDeviation) {
-					steps[i]++;
-				}
+//		double mean = calculateMean(magnitudes);
+//		double standardDeviation = calculateStandardDeviation(magnitudes, mean);
+//		for (int i = 1; i < magnitudes.length - 1; i++) {
+//			if (isPeak(magnitudes[i], magnitudes[i - 1], magnitudes[i + 1])) {
+//				if (Math.abs(magnitudes[i] - mean) > standardDeviation)
+//					steps++;
+//			}
+//		}
+		int n = 10;
+		System.out.println(magnitudes.length);
+		for(int i = n; i < magnitudes.length - n; i++){
+			double threshold = getThresholdForSection(magnitudes, i, n);
+			if (isPeak(magnitudes[i], magnitudes[i - 1], magnitudes[i + 1])) {
+				if (magnitudes[i] > threshold)steps++;
 			}
 		}
 		return steps;
-
 	}
 
-	public static double calculateMagnitude(double x, double y, double z) {
-		return Math.sqrt(x * x + y * y + z * z);
+	private static double calculateMagnitude(double x, double y, double z) {
+		return Math.sqrt((x * x) + (y * y) + (z * z));
 	}
 
 	public static double[] calculateMagnitudesFor(double[][] sensorData) {
@@ -34,21 +37,20 @@ public class StepCounter {
 
 	public static double calculateStandardDeviation(double[] arr, double mean) {
 		double standardDeviation = 0;
-		for (int i = 0; i < arr.length; i++) {
-			standardDeviation += (arr[i] - mean) * (arr[i] - mean);
+		for (double i : arr) {
+			standardDeviation += ((i - mean) * (i - mean));
 		}
-		standardDeviation = standardDeviation / (double) ((arr.length - 1) - 1);
-
+		standardDeviation /= (arr.length - 1);
 		return Math.sqrt(standardDeviation);
 	}
 
-	public static double calculateMean(double[] arr) {
-		int mean = 0;
-		for (int i = 0; i < arr.length; i++) {
-			mean += arr[i];
+	private static double calculateMean(double[] arr) {
+		double sum = 0;
+		for (double i : arr) {
+			sum += i;
 		}
-
-		return (double) mean / (double) arr.length;
+		double mean = sum / arr.length;
+		return mean;
 	}
 
 	/****
@@ -59,30 +61,28 @@ public class StepCounter {
 	 * @param magnitudes
 	 * @return whether an index is considered a peak
 	 */
-	public static boolean isPeak(double[] arr, int index) {
-		if (arr[index] > arr[index - 1] && arr[index] > arr[index + 1]) {
+
+	private static boolean isPeak(double possiblePeak, double valueBefore, double valueAfter) {
+		if (possiblePeak > valueBefore && possiblePeak > valueAfter)
 			return true;
-		}
 		return false;
 	}
 
-	public static int getNumSteps(double[] arr) {
-		int count = 0;
-		for (int i = 0; i < arr.length; i++) {
-			if (arr[i] == 1) {
-				count++;
-			}
-		}
-
-		return count;
-	}
-	
-	public static double getThresholdForSection(double[] arr, int startingIndex, int n) {
+	/***
+	 * Calculates the threshold for n data points left and right of current
+	 * value
+	 * 
+	 * @param startingIndex
+	 *            index that we are finding threshold for
+	 * @param n
+	 *            values left and right of startingIndex to use in window
+	 * @return threshold for this window
+	 */
+	private static double getThresholdForSection(double[] arr, int startingIndex, int n) {
 		double[] magnitudes = CSVData.getPartOfArray(arr, startingIndex - n, startingIndex + n);
 		double mean = calculateMean(magnitudes);
 		double standardDeviation = calculateStandardDeviation(magnitudes, mean);
 		double threshold = mean + standardDeviation;
 		return threshold;
 	}
-
 }
